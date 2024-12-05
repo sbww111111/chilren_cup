3982a281a7b4421d106c6a034e20d5f10f0cba67
“Natsume” <1396798294@qq.com>
2024-12-05 22:53:31 +0800

    科普

--- pages/knowledge/knowledge.vue
+++ pages/knowledge/knowledge.vue
@@ -1,16 +1,22 @@
 <template>
 	<view class="knowledge_container">
 		<view class="head">
-			<view class="search">
-				<input type="text" placeholder="输入关键字" />
-			</view>
-			<view class="search_btn">
-				<button>搜索</button>
-			</view>
+			<form @submit="formSubmit">
+				<view class="search">
+					<image class="search_span" src="/static/knowledge/water_drop.png"></image>
+					<input type="text" placeholder="输入关键字" :value="value" />
+				</view>
+				<view class="search_btn">
+					<button form-type="submit">搜索</button>
+				</view>
+			</form>
 		</view>
 		<view class="knowledge_posts">
 			<scroll-view scroll-y="true" class="knowledge_post_scroll">
-				<view class="knowledge_post_item" v-for="(item, index) in posts" :key="index"></view>
+				<view class="knowledge_post_item" v-for="(item, index) in posts" :key="index">
+					<image :src="item.postCoverUrl"></image>
+					<text>{{item.postTitle}}</text>
+				</view>
 				<!-- 空白占位 -->
 				<view style="width: 100%; height: 120px; float: left;"></view>
 			</scroll-view>
@@ -28,31 +34,33 @@
 		data() {
 			return {
 				posts: [{
-					postTitle: "标题",
-					postCoverUrl: ""
+					postTitle: "标题1",
+					postCoverUrl: "/static/knowledge/drink.png"
 				}, {
-					postTitle: "标题",
-					postCoverUrl: ""
+					postTitle: "标题2",
+					postCoverUrl: "/static/knowledge/drink.png"
 				}, {
-					postTitle: "标题",
-					postCoverUrl: ""
+					postTitle: "标题3",
+					postCoverUrl: "/static/knowledge/drink.png"
 				}, {
-					postTitle: "标题",
-					postCoverUrl: ""
+					postTitle: "标题4",
+					postCoverUrl: "/static/knowledge/drink.png"
 				}, {
-					postTitle: "标题",
-					postCoverUrl: ""
+					postTitle: "标题5",
+					postCoverUrl: "/static/knowledge/drink.png"
 				}, {
-					postTitle: "标题",
-					postCoverUrl: ""
+					postTitle: "标题6",
+					postCoverUrl: "/static/knowledge/drink.png"
 				}, {
-					postTitle: "标题",
-					postCoverUrl: ""
+					postTitle: "标题7",
+					postCoverUrl: "/static/knowledge/drink.png"
 				}]
 			}
 		},
 		methods: {
-
+			formSubmit: function(e) {
+				console.log('form发生了submit事件')
+			},
 		}
 	}
 </script>
@@ -77,6 +85,14 @@
 			border-radius: 30rpx;
 			padding-left: 50rpx;
 			margin-right: 10px;
+
+			.search_span {
+				position: absolute;
+				top: 7px;
+				left: 10px;
+				width: 15px;
+				height: 18px;
+			}
 		}
 
 		.search_btn {
@@ -117,6 +133,15 @@
 				border: solid 0;
 				border-radius: 20rpx;
 				background-color: #fffcd9;
+
+				image {
+					width: 100%;
+					height: 80%;
+				}
+
+				text {
+					padding: 5%;
+				}
 			}
 		}
 	}
