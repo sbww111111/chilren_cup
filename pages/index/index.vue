1798dc92ec599d468598bb7062930f4ca776f1e5
ftjynlxx <11308805+sulijie@user.noreply.gitee.com>
2024-12-09 13:40:05 +0800

    update CustomBar

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,4 +1,7 @@
 <template>
+	<view class="custom-nav-bar">
+	  <!-- <text class="custom-title">首页</text> -->
+	</view>
   <view class="container">
     <!-- 页面内容 -->
     <view class="page-content">欢迎来到首页</view>
@@ -15,4 +18,28 @@
 		CustomTabBar
 	  }
 	};
+	
 </script>
+<style>
+	.custom-nav-bar {
+	  width: 100%;
+	  height: 95px;
+	  background-color: #c3d1ec;
+	  display: flex;
+	  justify-content: center;  /* 让子元素水平居中 */
+	  align-items: center;  /* 让子元素垂直居中 */
+	  position: relative;
+	}
+	
+	.custom-title {
+	  color: #000000;
+	  font-size: 18px;
+	  
+	}
+	
+	
+	.container {
+	  padding-top: 10px;  /* 留出空间给自定义导航栏 */
+	}
+
+</style>
