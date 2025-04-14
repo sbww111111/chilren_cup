8209965ddd061307f12fcc3f46fd73b3bd5594b1
comxd <2273522480@qq.com>
2025-04-14 14:19:34 +0800

    222

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
 	<view class="bcground">
 		<view class="uni-margin-wrap">
 			<swiper class="swiper" circular :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval"
@@ -46,18 +45,7 @@
 		<!-- 自定义底部导航栏 -->
 		<CustomTabBar></CustomTabBar>
 	</view>
-=======
-	<view class="custom-nav-bar">
-	  <!-- <text class="custom-title">首页</text> -->
-	</view>
-  <view class="container">
-    <!-- 页面内容 -->
-    <view class="page-content">欢迎来到首页</view>
-    <!-- 自定义底部导航栏 -->
-    <CustomTabBar></CustomTabBar>
-  </view>
->>>>>>> 581a77042861427ffa08d69a5aacdd3fdb7b418c
-</template>、
+</template>
 
 <script>
 	import CustomTabBar from '@/components/CustomTabBar.vue';
@@ -82,7 +70,6 @@
 	};
 	
 </script>
-<<<<<<< HEAD
 
 <style>
 	.bcground {
@@ -168,28 +155,3 @@
 		left: 30%;
 	}
 </style>
-=======
-<style>
-	.custom-nav-bar {
-	  width: 100%;
-	  height: 95px;
-	  background-color: #c3d1ec;
-	  display: flex;
-	  justify-content: center;  /* 让子元素水平居中 */
-	  align-items: center;  /* 让子元素垂直居中 */
-	  position: relative;
-	}
-	
-	.custom-title {
-	  color: #000000;
-	  font-size: 18px;
-	  
-	}
-	
-	
-	.container {
-	  padding-top: 10px;  /* 留出空间给自定义导航栏 */
-	}
-
-</style>
->>>>>>> 581a77042861427ffa08d69a5aacdd3fdb7b418c
