f48bedb81916849b81e31afceda529fd4cfe09b8
“Natsume” <1396798294@qq.com>
2025-04-14 15:06:14 +0800

    xx

--- components/CustomTabBar.vue
+++ components/CustomTabBar.vue
@@ -11,44 +11,21 @@
 			<image class="tab-img" src="/static/fonts/community_1.png" />
 		</view>
 
-
-<<<<<<< HEAD
 		<!-- 中间按钮 -->
 		<view class="tab-item tab-item-center" @click="navigateTo('/pages/post/post')">
 			<image class="icon" src="/static/icons/points.png" />
 			<image class="tab-img" src="/static/fonts/home.png" />
 		</view>
-=======
-      <!-- 中间按钮 -->
-      <view class="tab-item tab-item-center" @click="navigateTo('/pages/index/index')">
-        <image class="icon" src="/static/icons/points.png" />
-		<image class="tab-img" src="/static/fonts/home.png"/>
-      </view>
-      
-      <!-- 右侧按钮 -->
-      <view class="tab-item tab-item-four" @click="navigateTo('/pages/index/index')">
-        <image class="icon" src="/static/icons/knowledge.png" />
-		<image class="tab-img" src="/static/fonts/home.png"/>
-      </view>
-	  <view class="tab-item tab-item-five" @click="navigateTo('/pages/my/my')">
-	    <image class="icon" src="/static/icons/my.png" />
-		<image class="tab-img" src="/static/fonts/home.png"/>
-	  </view>
-    </view>
->>>>>>> my
-
 		<!-- 右侧按钮 -->
 		<view class="tab-item tab-item-four" @click="navigateTo('/pages/knowledge/knowledge')">
 			<image class="icon" src="/static/icons/knowledge.png" />
 			<image class="tab-img" src="/static/fonts/home.png" />
 		</view>
-		<view class="tab-item tab-item-five" @click="navigateTo('/pages/user/user')">
+		<view class="tab-item tab-item-five" @click="navigateTo('/pages/my/my')">
 			<image class="icon" src="/static/icons/my.png" />
 			<image class="tab-img" src="/static/fonts/home.png" />
 		</view>
 	</view>
-
-<<<<<<< HEAD
 </template>
 
 <script>
@@ -62,19 +39,6 @@
 		}
 	};
 </script>
-=======
-  <script>
-  export default {
-    methods: {
-      navigateTo(url) {
-        uni.redirectTo({
-          url
-        });
-      }
-    }
-  };
-  </script>
->>>>>>> my
 
 
 <style scoped>
@@ -117,8 +81,7 @@
 		z-index: 1001;
 	}
 
-<<<<<<< HEAD
-	.tab-img {
+.tab-img {
 		position: absolute;
 		bottom: -10px;
 		/* 调整文字距离按钮的距离 */
@@ -168,20 +131,3 @@
 		height: 30px;
 	}
 </style>
-=======
-  /* 右侧按钮 */
-  .tab-item-four {
-    right: 23%;
-    transform: translateY(-110%); /* 超出半圆顶部 */
-  }
-  .tab-item-five {
-    right: 5%;
-    transform: translateY(-90%); /* 超出半圆顶部 */
-  }
-  /* 按钮图标 */
-  .icon {
-    width: 30px;
-    height: 30px;
-  }
-  </style>
->>>>>>> my
